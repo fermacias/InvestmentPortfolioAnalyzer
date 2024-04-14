a4fe9ed4704b9793e9b78fc2caa30f09925acf08
Fernanda Macías <fernanda.macias@ug.uchile.cl>
2024-04-13 23:21:52 -0400

    feat: receives the initial values from the user
    the code receives the start data, the final data and the amount from the user, and makes sure there are in the correct format

--- app/main.rb
+++ app/main.rb
@@ -9,15 +9,12 @@ def get_input_from_user
     start_date = nil
     end_date = nil
     loop do
-        start_date = "05-01-2020"
-        #start_date = request_valid_date("Enter the investment start date")
-        end_date = "20-01-2020"
-        #end_date = request_valid_date("Enter the investment end date")
+        start_date = request_valid_date("Enter the investment start date")
+        end_date = request_valid_date("Enter the investment end date")
         break if Date.parse(start_date) < Date.parse(end_date)
         puts "The end date must be after the start date."
     end
-    investment_amount = 20000
-    #investment_amount = request_valid_integer("Enter the investment amount")
+    investment_amount = request_valid_integer("Enter the investment amount")
     return start_date, end_date, investment_amount
 end
 
